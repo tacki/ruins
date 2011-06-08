8b724a12c421278312c858903e168a26ad13abf3
Markus Schlegel <g42@gmx.net>
2011-06-08 09:46:24 +0200

    Initial Setup (main) fixed
    Change-Id: I35d886caf37bdae21e15e6cda2d35fd4f3dfa28c

--- main/setup/install.php
+++ main/setup/install.php
@@ -1,6 +1,8 @@
 <?php
 global $em;
 
+use Manager\Item;
+
 // Create Test-Character
 if (!($install_char = $em->getRepository("Entities\Character")->findOneByName("Testuser"))) {
     $install_char = new Entities\Character;
@@ -29,65 +31,40 @@ if (!($install_user = $em->getRepository("Entities\User")->findOneByLogin("test"
 
 // Create Test-Weapons
 if (!($weapon = $em->getRepository("Entities\Items\Weapon")->findOneByName("Testwaffe"))) {
-    $weapon = new Entities\Items\Weapon;
-    $weapon->class = Manager\Item::CLASS_WEAPON;
-    $weapon->name = "Testwaffe";
+    $weapon             = new Entities\Items\Weapon;
+    $weapon->class      = Item::CLASS_WEAPON;
+    $weapon->name       = "Testwaffe";
     $weapon->damage_min = 5;
     $weapon->damage_max = 10;
-    $weapon->location = Manager\Item::LOCATION_BACKPACK;
+    $weapon->location   = Item::LOCATION_BACKPACK;
+    $weapon->owner      = $install_char;
     $em->persist($weapon);
 }
 
-// Create Test-Armor (Head)
-if (!($armor_head = $em->getRepository("Entities\Items\Armor")->findOneByName("Testarmor"))) {
-    $armor_head = new Entities\Items\Armor;
-    $armor_head->class = Manager\Item::CLASS_ARMOR_HEAD;
-    $armor_head->name = "Testrüstung (Kopf)";
-    $em->persist($armor_head);
-}
+// Create Test-Armor
+$armors = array (
+                   Item::CLASS_ARMOR_HEAD  => "Testrüstung (Kopf)",
+                   Item::CLASS_ARMOR_ARMS  => "Testrüstung (Arm)",
+                   Item::CLASS_ARMOR_CHEST => "Testrüstung (Brust)",
+                   Item::CLASS_ARMOR_LEGS  => "Testrüstung (Beine)",
+                   Item::CLASS_ARMOR_FEET  => "Testrüstung (Füße)",
+                );
 
-// Create Test-Armor (Arms)
-if (!($armor_arms = $em->getRepository("Entities\Items\Armor")->findOneByName("Testarmor"))) {
-    $armor_arms = new Entities\Items\Armor;
-    $armor_arms->class = Manager\Item::CLASS_ARMOR_ARMS;
-    $armor_arms->name = "Testrüstung (Arm)";
-    $em->persist($armor_arms);
-}
-
-// Create Test-Armor (Chest)
-if (!($armor_chest = $em->getRepository("Entities\Items\Armor")->findOneByName("Testarmor"))) {
-    $armor_chest = new Entities\Items\Armor;
-    $armor_chest->class = Manager\Item::CLASS_ARMOR_CHEST;
-    $armor_chest->name = "Testrüstung (Brust)";
-    $em->persist($armor_chest);
-}
-
-// Create Test-Armor (Legs)
-if (!($armor_legs = $em->getRepository("Entities\Items\Armor")->findOneByName("Testarmor"))) {
-    $armor_legs = new Entities\Items\Armor;
-    $armor_legs->class = Manager\Item::CLASS_ARMOR_LEGS;
-    $armor_legs->name = "Testrüstung (Beine)";
-    $em->persist($armor_legs);
-}
-
-// Create Test-Armor (Feet)
-if (!($armor_feet = $em->getRepository("Entities\Items\Armor")->findOneByName("Testarmor"))) {
-    $armor_feet = new Entities\Items\Armor;
-    $armor_feet->class = Manager\Item::CLASS_ARMOR_FEET;
-    $armor_feet->name = "Testrüstung (Füße)";
-    $em->persist($armor_feet);
+foreach ($armors as $armorclass => $armorname) {
+    if (!($armor = $em->getRepository("Entities\Items\Armor")->findOneByName($armorname))) {
+        $armor             = new Entities\Items\Armor;
+        $armor->class      = $armorclass;
+        $armor->name       = $armorname;
+        $armor->armorclass = 1;
+        $armor->location   = Item::LOCATION_BACKPACK;
+        $armor->owner      = $install_char;
+        $em->persist($armor);
+    }
 }
 
 
 // Reverse Mappings
 $install_char->user     = $install_user;
 $install_settings->user = $install_user;
-
-$weapon->owner          = $install_char;
-$armor_head->owner      = $install_char;
-$armor_arms->owner      = $install_char;
-$armor_chest->owner     = $install_char;
-$armor_legs->owner      = $install_char;
-$armor_feet->owner      = $install_char;
 
 ?>
