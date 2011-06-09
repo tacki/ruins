2e977d650e2f4d1f1857207c3e8eb1002a170146
Markus Schlegel <g42@gmx.net>
2011-06-09 16:54:24 +0200

    OpenID separated from PEAR-Directory - OpenID moved to Manager Time to get rid of PEAR
    Change-Id: Icd0b23b0098337e7a30069907fb767d2e0a7942f

---
+++ common/external/php-openid_init.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * PHP-OpenID Initialization
+ *
+ * @author Markus Schlegel <g42@gmx.net>
+ * @copyright Copyright (C) 2011 Markus Schlegel
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package Ruins
+ */
+
+$path_extra = dirname(__FILE__) . "/php-openid";
+$path = ini_get('include_path');
+$path = $path_extra . PATH_SEPARATOR . $path;
+ini_set('include_path', $path);
+
+require_once "Auth/OpenID/Consumer.php";
+require_once "Auth/OpenID/FileStore.php";
+require_once "Auth/OpenID/SReg.php";
+require_once "Auth/OpenID/PAPE.php";
+
+global $pape_policy_uris;
+$pape_policy_uris = array(
+    PAPE_AUTH_MULTI_FACTOR_PHYSICAL,
+    PAPE_AUTH_MULTI_FACTOR,
+    PAPE_AUTH_PHISHING_RESISTANT
+);
+?>
